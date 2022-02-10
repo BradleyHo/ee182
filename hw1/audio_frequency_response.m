a6b366ee294170f13472d0a63b40050429296399
Bradley Ho <BradleyHo@users.noreply.github.com>
2022-02-09 21:09:26 -0800

    Update audio_frequency_response.m

--- hw1/audio_frequency_response.m
+++ hw1/audio_frequency_response.m
@@ -1,13 +1,13 @@
 clc; clear;
 
-[x, fs] = audioread('bradleytylerho.wav');
+[x, fs] = audioread('bradleytylerho.wav'); # Provide your own audio signal
 
 N = length(x);
 n = 0 : N - 1;
 plot(n, x)
 
 OM = 0 : 0.01 : pi;
-X = exp(-j * OM' * n) * x;
+X = exp(-1i * OM' * n) * x; # MATLAB recommends to change j and i to 1i
 
 f = OM * fs / (2 * pi);
 figure;
